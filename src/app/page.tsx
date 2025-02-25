672823b31bc29b35963ed27a79cdb583180fbf7b
Inesh Dey <ineshdey@hotmail.com>
2025-02-24 18:43:25 -0800

    color fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
         </div>
       </div>
       {/* Page Section */}
-      <div className="bg-accent lg:h-96 w-full text-black p-16 z-20" style={{ boxShadow: "0 -10px 30px rgba(0, 0, 0, 0.22)" }}>
+      <div className="bg-black lg:h-96 w-full text-background p-16 z-20" style={{ boxShadow: "0 -10px 30px rgba(0, 0, 0, 0.22)" }}>
         <div className="flex flex-col-reverse lg:flex-row flex-1 h-full max-w-screen-2xl mx-auto gap-16">
           <div className='w-full lg:w-1/3 flex flex-col justify-center h-full max-lg:text-center'>
             <motion.div

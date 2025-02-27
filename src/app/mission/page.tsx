0486d94118c1bb280ab51c86c694c682210aeb23
sath08 <167156060+sath08@users.noreply.github.com>
2025-02-27 00:53:21 -0800

    Initial Commit
    Creation of Files

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
-  return (
-    <div>Mission</div>
-  )
-}
+  return <div>Mission</div>;
+};
 
-export default Page
+export default Page;

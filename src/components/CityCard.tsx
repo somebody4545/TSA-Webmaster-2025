4c63127a6d1c2fcca35fbff4348cc0b351e2bfb9
Anirudh Narasimhan <thenoobdude21.12@gmail.com>
2025-03-01 09:11:40 -0800

    continue work on locations!!! (im moving to pc)

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,36 +1,44 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import { ChevronRight } from 'lucide-react'
+import React from "react";
+import Image from "next/image";
+import { ChevronRight } from "lucide-react";
+import { motion } from "framer-motion";
 
-const CityCard = ({ city, image }) => {
-    const handleArrowClick = () => {
-      // Your logic here, for example:
-      console.log(city);
-      // Or navigate to a city page
-      // router.push(`/cities/${city}`);
-    };
-  
-    return (
-      <div className="bg-primary-darkest rounded-lg overflow-hidden relative">
+const CityCard = ({ city, image, onArrowClick, isSelected }) => {
+  const handleArrowClick = () => {
+    onArrowClick(city);
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div
+        className={`bg-primary-darkest rounded-lg overflow-hidden relative ${
+          isSelected ? "z-10 ring-2 ring-white" : ""
+        }`}
+      >
         <h3 className="text-2xl text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
           {city}
         </h3>
-        <Image 
+        <Image
           src={image}
           alt={city}
           layout="fill"
           className="object-cover opacity-10"
         />
-        <div 
+        <div
           className="absolute top-1/2 right-5 transform -translate-y-1/2 cursor-pointer text-white"
           onClick={handleArrowClick}
         >
           <ChevronRight />
         </div>
       </div>
-    );
-  };
+    </motion.div>
+  );
+};
 
 export default CityCard;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
-const containerStyle = {
-    width: "100%",
-    height: "400px",
-};
-
-
-const center = {
-    lat: 47.6062, // Example: Seattle, WA
-    lng: -122.3321,
-};
-
-const Map = ({city, address}) => {
-    const api_key = "AIzaSyD7T3QffKqS2HdHVFbBgRes-mN93cat3Lo"
+const Map = ({city}) => {
+    const cityMaps = {
+        "New York":
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.11976397304903!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sus!4v1614799014016!5m2!1sen!2sus",
+        "Los Angeles":
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423286.27405770525!2d-118.69192047471653!3d34.02016130653036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sus!4v1614799058082!5m2!1sen!2sus",
+        "Chicago":
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d190255.33858082378!2d-87.87204681640622!3d41.83390306762297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c3cd0f4cbed%3A0xafe0a6ad09c0c000!2sChicago%2C%20IL%2C%20USA!5e0!3m2!1sen!2sus!4v1614799114155!5m2!1sen!2sus",
+        "Seattle":
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172139.0895319106!2d-122.48214643493939!3d47.61294318273676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490102c93e83355%3A0x102565466944d59a!2sSeattle%2C%20WA%2C%20USA!5e0!3m2!1sen!2sus!4v1614799149080!5m2!1sen!2sus",
+      };
     return (
-        <LoadScript googleMapsApiKey={api_key}>
-          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} />
-        </LoadScript>
+        <div className="w-full h-4/5">
+            <h1 className='font-heading text-3xl text-text text-center'>{city}</h1>
+            <iframe
+                src={cityMaps[city]}
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                title={`Map of ${city}`}
+            ></iframe>
+        </div>  
     );
 }
 

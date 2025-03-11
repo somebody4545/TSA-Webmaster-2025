becae9421e2e81f4aaf24e5b95a27e3377f0feeb
Inesh Dey <ineshdey@hotmail.com>
2025-03-10 20:24:43 -0700

    feat: scroll down indicator for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,6 +231,30 @@ function Hero() {
       >
         1. Green in Malagasy. <br /><span className="font-bold">2. A plant-based experience from every cuisine.</span>
       </motion.p>
+      <motion.div
+        className="absolute bottom-8 animate-bounce"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
+        <p className="text-sm text-center">Scroll Down</p>
+        <div className="flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-chevron-down"
+          >
+            <polyline points="6 9 12 15 18 9" />
+          </svg>
+        </div>
+      </motion.div>
     </div>
   );
 }

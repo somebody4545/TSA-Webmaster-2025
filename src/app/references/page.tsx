a2c3d0fe211dd998d7bb69d701773603600d9833
Inesh Dey <ineshdey@hotmail.com>
2025-03-13 21:10:33 -0700

    Update page.tsx

--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -36,7 +36,7 @@ export default function ReferencesPage() {
       <h1 className="text-2xl font-bold mb-4">References</h1>
       <div>
         <h2 className="text-xl font-semibold mb-2">Code Stack</h2>
-        <p>This website utilizes NextJS, a modern framework built on top of ReactJS optimized for efficiency and fast render times. On top of this, this website utilizes TailwindCSS, a framework allowing for shorthand CSS to be written directly in components, as well as DaisyUI, a TailwindCSS addition with accessible and responsive class names for standard components. This site follows current WCAG accessibility guidelines for color contrast, with small text at a AAA contrast ratio rating, and large text with at least a AA contrast ratio rating. Card renders are made with Twinmotion, Adobe Illustrator, and Blender.</p>
+        <p>This website utilizes NextJS, a modern framework built on top of ReactJS optimized for efficiency and fast render times. On top of this, this website utilizes TailwindCSS, a framework allowing for shorthand CSS to be written directly in components, as well as DaisyUI, a TailwindCSS addition with accessible and responsive class names for standard components. Non-standard components and the theming of the site are done by the work of our team. This site follows current WCAG accessibility guidelines for color contrast, with small text at a AAA contrast ratio rating, and large text with at least a AA contrast ratio rating. Card renders are made with Twinmotion, Adobe Illustrator, and Blender.</p>
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-2">Additional Libraries Utilized</h2>

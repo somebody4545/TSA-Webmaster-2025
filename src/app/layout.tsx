dbba31cbc11ef9724c7531f3f95be523cfd9b5d8
Inesh Dey <ineshdey@hotmail.com>
2025-07-01 18:13:00 -0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Maitso - Plant Powered',
@@ -21,6 +22,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
